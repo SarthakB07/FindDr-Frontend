c1a991b955d19b1cc11a9233e9c4eb6095bdea2d
SarthakB07 <sarthak.bansal@spit.ac.in>
2024-04-05 14:57:12 +0530

    address edited in footer

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -68,15 +68,15 @@ const Footer = () => {
             <h4>Contact</h4>
             <div>
               <FaPhone />
-              <span>999-999-9999</span>
+              <span>9784-XXX-XXXX</span>
             </div>
             <div>
               <MdEmail />
-              <span>zeelab@gmail.com</span>
+              <span>BansalHospital@gmail.com</span>
             </div>
             <div>
               <FaLocationArrow />
-              <span>Karachi, Pakistan</span>
+              <span>Jaipur, Rajasthan</span>
             </div>
           </div>
         </div>
