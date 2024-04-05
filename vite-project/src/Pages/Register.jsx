9160c81f7bcd2547029785c4afb69e03fa2147d5
SarthakB07 <sarthak.bansal@spit.ac.in>
2024-04-05 15:21:33 +0530

    Login and Register api changed

--- vite-project/src/Pages/Register.jsx
+++ vite-project/src/Pages/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/patient/register",
+          "http://localhost:8000/api/v1/user/patient/register",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
